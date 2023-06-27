d885beb75b558a493b05727ba9cc56267f65e874
Daniyar Itegulov <ditegulov@gmail.com>
2023-06-27 11:12:32 +1000

    more mio logs

--- mio/src/sys/unix/uds/stream.rs
+++ mio/src/sys/unix/uds/stream.rs
@@ -1,3 +1,5 @@
+use log::debug;
+
 use super::{socket_addr, SocketAddr};
 use crate::sys::unix::net::new_socket;
 
@@ -7,15 +9,27 @@ use std::os::unix::net;
 use std::path::Path;
 
 pub(crate) fn connect(path: &Path) -> io::Result<net::UnixStream> {
+    debug!("mio uds connect start");
     let (sockaddr, socklen) = socket_addr(path)?;
+    debug!("mio uds connect addr 1: {:?} {}", sockaddr, socklen);
     let sockaddr = &sockaddr as *const libc::sockaddr_un as *const libc::sockaddr;
+    debug!("mio uds connect addr 2: {:?}", sockaddr);
 
     let fd = new_socket(libc::AF_UNIX, libc::SOCK_STREAM)?;
+    debug!("mio uds connect fd: {:?}", fd);
     let socket = unsafe { net::UnixStream::from_raw_fd(fd) };
+    debug!("mio uds connect socket: {:?}", socket);
     match syscall!(connect(fd, sockaddr, socklen)) {
-        Ok(_) => {}
-        Err(ref err) if err.raw_os_error() == Some(libc::EINPROGRESS) => {}
-        Err(e) => return Err(e),
+        Ok(_) => {
+            debug!("mio uds connect ok");
+        }
+        Err(ref err) if err.raw_os_error() == Some(libc::EINPROGRESS) => {
+            debug!("mio uds connect error 1: {:?}", err);
+        }
+        Err(e) => {
+            debug!("mio uds connect error 2: {:?}", e);
+            return Err(e);
+        }
     }
 
     Ok(socket)
