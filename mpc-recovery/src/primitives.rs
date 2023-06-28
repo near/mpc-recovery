a220cc5d7e2ad873d1e0c703c94fe2f035e58de1
Serhii Volovyk <sergeyvolovyk@gmail.com>
2023-06-28 13:02:45 +0000

    OidcDigest seiralization tests added

--- mpc-recovery/src/primitives.rs
+++ mpc-recovery/src/primitives.rs
@@ -112,3 +112,49 @@ impl OidcDigest {
         format!("{}/{}", self.node_id, hex::encode(self.digest))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::utils::{claim_oidc_request_digest, oidc_digest};
+
+    use super::*;
+
+    #[test]
+    fn test_oidc_digest_from_and_to_value() {
+        let public_key = "ed25519:J75xXmF7WUPS3xCm3hy2tgwLCKdYM1iJd4BWF8sWVnae".to_string();
+        let oidc_token = "validToken:oR8hig9XkU";
+
+        let oidc_token_hash = oidc_digest(&oidc_token);
+
+        let digest = match claim_oidc_request_digest(oidc_token_hash) {
+            Ok(digest) => digest,
+            Err(err) => panic!("Failed to create digest: {:?}", err),
+        };
+
+        let digest_32 = <[u8; 32]>::try_from(digest).expect("Hash was wrong size");
+
+        let oidc_digest = OidcDigest {
+            node_id: 1,
+            digest: digest_32,
+            public_key: public_key.parse().expect("Failed to parse public key"),
+        };
+
+        let val = oidc_digest.clone().into_value();
+
+        let reconstructed_oidc_digest = match OidcDigest::from_value(val) {
+            Ok(oidc_digest) => oidc_digest,
+            Err(err) => panic!("Failed to reconstruct OidcDigest: {:?}", err),
+        };
+
+        // Wrong digest for comparison
+        let public_key_2 = "ed25519:EBNJGHctB2LuDsCyMWrfwW87QrAob2kKzoS98PR5vjJn";
+        let oidc_digest_2 = OidcDigest {
+            node_id: 1,
+            digest: digest_32,
+            public_key: public_key_2.parse().expect("Failed to parse public key"),
+        };
+
+        assert_eq!(oidc_digest, reconstructed_oidc_digest);
+        assert_ne!(oidc_digest_2, reconstructed_oidc_digest);
+    }
+}
