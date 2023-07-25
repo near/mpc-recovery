cee9cb9dcd9a8709d11890c7613c0c0c73f4ba49
Phuong N <phuong@near.org>
2023-07-25 22:46:44 +0000

    Moved test functions over to mod.rs

--- integration-tests/tests/mpc/mod.rs
+++ integration-tests/tests/mpc/mod.rs
@@ -1,2 +1,94 @@
+use std::str::FromStr;
+use std::time::Duration;
+
+use mpc_recovery::msg::{NewAccountResponse, UserCredentialsResponse};
+use near_crypto::{PublicKey, SecretKey};
+use workspaces::AccountId;
+
+use crate::{account, check, key, token, MpcCheck, TestContext};
+
 mod negative;
 mod positive;
+
+pub async fn new_random_account(
+    ctx: &TestContext<'_>,
+) -> anyhow::Result<(AccountId, SecretKey, PublicKey, String)> {
+    let account_id = account::random(ctx.worker)?;
+    let user_secret_key = key::random_sk();
+    let user_public_key = user_secret_key.public_key();
+    let oidc_token = token::valid_random();
+
+    // Claim OIDC token
+    ctx.leader_node
+        .claim_oidc_with_helper(
+            oidc_token.clone(),
+            user_public_key.clone(),
+            user_secret_key.clone(),
+        )
+        .await?;
+
+    // Create account
+    let new_acc_response = ctx
+        .leader_node
+        .new_account_with_helper(
+            account_id.clone().to_string(),
+            user_public_key.clone(),
+            None,
+            user_secret_key.clone(),
+            oidc_token.clone(),
+        )
+        .await?
+        .assert_ok()?;
+
+    assert!(matches!(new_acc_response, NewAccountResponse::Ok {
+            create_account_options: _,
+            user_recovery_public_key: _,
+            near_account_id: acc_id,
+        } if acc_id == account_id.to_string()
+    ));
+
+    Ok((account_id, user_secret_key, user_public_key, oidc_token))
+}
+
+pub async fn fetch_recovery_pk(
+    ctx: &TestContext<'_>,
+    user_sk: &SecretKey,
+    user_oidc: String,
+) -> anyhow::Result<PublicKey> {
+    let recovery_pk = match ctx
+        .leader_node
+        .user_credentials_with_helper(user_oidc.clone(), user_sk.clone(), user_sk.public_key())
+        .await?
+        .assert_ok()?
+    {
+        UserCredentialsResponse::Ok { recovery_pk } => PublicKey::from_str(&recovery_pk)?,
+        UserCredentialsResponse::Err { msg } => anyhow::bail!("error response: {}", msg),
+    };
+    Ok(recovery_pk)
+}
+
+pub async fn add_pk_and_check_validity(
+    ctx: &TestContext<'_>,
+    user_id: &AccountId,
+    user_sk: &SecretKey,
+    user_pk: &PublicKey,
+    user_oidc: String,
+    user_recovery_pk: &PublicKey,
+    pk_to_add: Option<String>,
+) -> anyhow::Result<String> {
+    let new_user_pk = pk_to_add.unwrap_or_else(key::random_pk);
+    ctx.leader_node
+        .add_key_with_helper(
+            user_id.clone(),
+            user_oidc,
+            new_user_pk.parse()?,
+            user_recovery_pk.clone(),
+            user_sk.clone(),
+            user_pk.clone(),
+        )
+        .await?
+        .assert_ok()?;
+    tokio::time::sleep(Duration::from_millis(2000)).await;
+    check::access_key_exists(ctx, user_id, &new_user_pk).await?;
+    Ok(new_user_pk)
+}
