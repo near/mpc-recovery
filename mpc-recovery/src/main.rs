81990a0180ac1e512455bd98271b44a16e42d52c
Phuong Nguyen <phuong@near.org>
2023-09-05 12:57:20 +0200

    Try terraform

--- mpc-recovery/src/main.rs
+++ mpc-recovery/src/main.rs
@@ -194,9 +194,10 @@ async fn load_oidc_providers(
         None => {
             let name =
                 format!("mpc-recovery-allowed-oidc-providers-{node_id}-{env}/versions/latest");
+            let x = std::str::from_utf8(&gcp_service.load_secret(name).await?)?.to_string();
+            tracing::info!("Loaded OIDC providers: {x:?}");
             let oidc_providers: Vec<mpc_recovery::firewall::allowed::OidcProvider> =
-                serde_json::from_str(std::str::from_utf8(&gcp_service.load_secret(name).await?)?)?;
-            tracing::info!("Loaded OIDC providers: {oidc_providers:?}");
+                serde_json::from_str(&x)?;
 
             Ok(AllowedOidcProviders {
                 entries: oidc_providers.into_iter().collect(),
@@ -375,4 +376,16 @@ async fn main() -> anyhow::Result<()> {
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use mpc_recovery::firewall::allowed::OidcProvider;
+
+    #[test]
+    fn test_() {
+        let x = "{\"issuer\":\"https://securetoken.google.com/pagoda-oboarding-dev\",\"audience\":\"pagoda-oboarding-dev\"}";
+        let y: OidcProvider = serde_json::from_str(&format!("\"{}\"", x)).unwrap();
+        println!("{:?}", y);
+    }
 }
