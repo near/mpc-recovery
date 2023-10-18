456fcc9994512aacc724da97a1e64517549c5133
Serhii Volovyk <SergeyVolovyk@gmail.com>
2023-10-18 17:35:06 +0300

    sign logic added to load tests

--- load-tests/src/main.rs
+++ load-tests/src/main.rs
@@ -7,13 +7,22 @@ use std::vec;
 use constants::VALID_OIDC_PROVIDER_KEY;
 use goose::prelude::*;
 use mpc_recovery::{
-    msg::{ClaimOidcRequest, MpcPkRequest, NewAccountRequest},
+    msg::{ClaimOidcRequest, MpcPkRequest, NewAccountRequest, SignRequest, UserCredentialsRequest},
     sign_node::oidc::OidcToken,
     transaction::CreateAccountOptions,
-    utils::{claim_oidc_request_digest, sign_digest, user_credentials_request_digest},
+    utils::{
+        claim_oidc_request_digest, sign_digest, sign_request_digest,
+        user_credentials_request_digest,
+    },
 };
 use near_crypto::SecretKey;
-use near_primitives::types::AccountId;
+use near_primitives::{
+    account::{AccessKey, AccessKeyPermission},
+    borsh::BorshSerialize,
+    delegate_action::DelegateAction,
+    transaction::{Action, AddKeyAction},
+    types::AccountId,
+};
 use primitives::UserSession;
 use rand::{distributions::Alphanumeric, Rng};
 use utils::build_send_and_check_request;
@@ -28,7 +37,20 @@ async fn main() -> Result<(), GooseError> {
                 .register_transaction(transaction!(new_account).set_sequence(3)),
         )
         .register_scenario(
+            scenario!("registrationAndSign")
+                .register_transaction(transaction!(prepare_user_credentials).set_sequence(1))
+                .register_transaction(transaction!(claim_oidc).set_sequence(2))
+                .register_transaction(transaction!(new_account).set_sequence(3))
+                .register_transaction(transaction!(user_credentials).set_sequence(4))
+                .register_transaction(transaction!(sign).set_sequence(5)),
+        )
+        .register_scenario(
             scenario!("simpleMpcPublicKey").register_transaction(transaction!(mpc_public_key)),
+        )
+        .register_scenario(
+            scenario!("simpleClaimOidc")
+                .register_transaction(transaction!(prepare_user_credentials).set_sequence(1))
+                .register_transaction(transaction!(claim_oidc).set_sequence(2)),
         )
         .execute()
         .await?;
@@ -96,8 +118,27 @@ async fn claim_oidc(user: &mut GooseUser) -> TransactionResult {
     build_send_and_check_request(user, "claim_oidc", &body_json).await
 }
 
-async fn _user_credentials(_user: &mut GooseUser) -> TransactionResult {
-    Ok(())
+async fn user_credentials(user: &mut GooseUser) -> TransactionResult {
+    let sesion = user.get_session_data::<UserSession>().unwrap();
+    let oidc_token = sesion.jwt_token.clone();
+    let fa_secret_key = sesion.fa_sk.clone();
+    let fa_public_key = fa_secret_key.public_key();
+
+    let user_credentials_request_digest =
+        user_credentials_request_digest(&oidc_token, &sesion.fa_sk.public_key()).unwrap();
+
+    let user_credentials_frp_signature =
+        sign_digest(&user_credentials_request_digest, &fa_secret_key).unwrap();
+
+    let user_credentials_request = UserCredentialsRequest {
+        oidc_token,
+        frp_public_key: fa_public_key,
+        frp_signature: user_credentials_frp_signature,
+    };
+
+    let body_json =
+        serde_json::to_string(&user_credentials_request).expect("json serialization failed");
+    build_send_and_check_request(user, "user_credentials", &body_json).await
 }
 
 async fn new_account(user: &mut GooseUser) -> TransactionResult {
@@ -116,11 +157,8 @@ async fn new_account(user: &mut GooseUser) -> TransactionResult {
     let user_credentials_request_digest =
         user_credentials_request_digest(&oidc_token, &fa_public_key).unwrap();
 
-    let user_credentials_frp_signature = match fa_secret_key.sign(&user_credentials_request_digest)
-    {
-        near_crypto::Signature::ED25519(k) => k,
-        _ => panic!("wrong signature type"),
-    };
+    let user_credentials_frp_signature =
+        sign_digest(&user_credentials_request_digest, &fa_secret_key).unwrap();
 
     let new_account_request = NewAccountRequest {
         near_account_id: user_account_id,
@@ -134,6 +172,54 @@ async fn new_account(user: &mut GooseUser) -> TransactionResult {
     build_send_and_check_request(user, "new_account", &body_json).await
 }
 
-async fn _sign(_user: &mut GooseUser) -> TransactionResult {
-    Ok(())
+async fn sign(user: &mut GooseUser) -> TransactionResult {
+    let session = user.get_session_data::<UserSession>().unwrap();
+    let oidc_token = session.jwt_token.clone();
+    let fa_secret_key = session.fa_sk.clone();
+    let fa_public_key = fa_secret_key.public_key();
+    let account_id = session.near_account_id.clone();
+
+    let new_secret_key = SecretKey::from_random(near_crypto::KeyType::ED25519);
+    let new_public_key = new_secret_key.public_key();
+
+    let nonce = 0; // TODO: get nonce from near node
+    let block_height = 0; // TODO: get block height from near node
+    let recovery_pk = fa_public_key.clone(); // TODO: get recovery_pk from near node
+
+    let add_key_delegate_action = DelegateAction {
+        sender_id: account_id.clone(),
+        receiver_id: account_id.clone(),
+        actions: vec![Action::AddKey(AddKeyAction {
+            public_key: new_public_key.clone(),
+            access_key: AccessKey {
+                nonce: 0,
+                permission: AccessKeyPermission::FullAccess,
+            },
+        })
+        .try_into()
+        .unwrap()],
+        nonce,
+        max_block_height: block_height + 100,
+        public_key: recovery_pk.clone(),
+    };
+
+    let sign_request_digest =
+        sign_request_digest(&add_key_delegate_action, &oidc_token, &fa_public_key).unwrap();
+    let sign_request_frp_signature = sign_digest(&sign_request_digest, &fa_secret_key).unwrap();
+
+    let user_credentials_request_digest =
+        user_credentials_request_digest(&oidc_token, &fa_public_key).unwrap();
+    let user_credentials_frp_signature =
+        sign_digest(&user_credentials_request_digest, &fa_secret_key).unwrap();
+
+    let sign_request = SignRequest {
+        delegate_action: add_key_delegate_action.try_to_vec().unwrap(),
+        oidc_token,
+        frp_signature: sign_request_frp_signature,
+        user_credentials_frp_signature,
+        frp_public_key: fa_public_key,
+    };
+
+    let body_json = serde_json::to_string(&sign_request).expect("json serialization failed");
+    build_send_and_check_request(user, "sign", &body_json).await
 }
