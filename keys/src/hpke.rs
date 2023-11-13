4d40f3d66d619ddae1ae26d4a552fc036292cae1
Phuong Nguyen <phuong@near.org>
2023-11-13 14:53:29 -0600

    Utilize HPKE serde feature flag

--- keys/src/hpke.rs
+++ keys/src/hpke.rs
@@ -1,5 +1,3 @@
-use std::sync::Arc;
-
 use borsh::{self, BorshDeserialize, BorshSerialize};
 use hpke::{
     aead::{AeadTag, ChaCha20Poly1305},
@@ -7,6 +5,7 @@ use hpke::{
     kem::X25519HkdfSha256,
     OpModeR,
 };
+use serde::{Deserialize, Serialize};
 
 /// This can be used to customize the generated key. This will be used as a sort of
 /// versioning mechanism for the key.
@@ -24,14 +23,17 @@ pub struct Ciphered {
     pub tag: Tag,
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct Tag(AeadTag<Aead>);
 
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct PublicKey(<Kem as hpke::Kem>::PublicKey);
 
 // NOTE: Arc is used to hack up the fact that the internal private key does not have Send constraint.
-#[derive(Clone, PartialEq, Eq)]
-pub struct SecretKey(Arc<<Kem as hpke::Kem>::PrivateKey>);
+#[derive(Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct SecretKey(<Kem as hpke::Kem>::PrivateKey);
+
+#[derive(Clone, Serialize, Deserialize)]
 pub struct EncappedKey(<Kem as hpke::Kem>::EncappedKey);
 
 // Series of bytes that have been previously encoded/encrypted.
@@ -78,25 +80,6 @@ impl PublicKey {
     }
 }
 
-impl serde::Serialize for PublicKey {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        serializer.serialize_bytes(&self.to_bytes())
-    }
-}
-
-impl<'de> serde::Deserialize<'de> for PublicKey {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        PublicKey::try_from_bytes(&<Vec<u8> as serde::Deserialize>::deserialize(deserializer)?)
-            .map_err(|err| serde::de::Error::custom(format!("invalid HPKE public key: {err:?}")))
-    }
-}
-
 impl BorshSerialize for PublicKey {
     fn serialize<W: std::io::Write>(&self, writer: &mut W) -> std::io::Result<()> {
         BorshSerialize::serialize(&self.to_bytes(), writer)
@@ -113,11 +96,11 @@ impl BorshDeserialize for PublicKey {
 
 impl SecretKey {
     pub fn to_bytes(&self) -> [u8; 32] {
-        hpke::Serializable::to_bytes(self.0.as_ref()).into()
+        hpke::Serializable::to_bytes(&self.0).into()
     }
 
     pub fn from_bytes(bytes: &[u8]) -> Result<Self, hpke::HpkeError> {
-        Ok(Self(Arc::new(hpke::Deserializable::from_bytes(bytes)?)))
+        Ok(Self(hpke::Deserializable::from_bytes(bytes)?))
     }
 
     pub fn decrypt(&self, cipher: &Ciphered, associated_data: &[u8]) -> Vec<u8> {
@@ -140,56 +123,8 @@ impl SecretKey {
     }
 }
 
-impl serde::Serialize for EncappedKey {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        serializer.serialize_bytes(&hpke::Serializable::to_bytes(&self.0))
-    }
-}
-
-impl<'de> serde::Deserialize<'de> for EncappedKey {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        Ok(Self(
-            hpke::Deserializable::from_bytes(&<Vec<u8> as serde::Deserialize>::deserialize(
-                deserializer,
-            )?)
-            .map_err(|err| {
-                serde::de::Error::custom(format!("invalid HPKE encapped key: {err:?}"))
-            })?,
-        ))
-    }
-}
-
-impl serde::Serialize for Tag {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        serializer.serialize_bytes(&hpke::Serializable::to_bytes(&self.0))
-    }
-}
-
-impl<'de> serde::Deserialize<'de> for Tag {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        Ok(Tag(hpke::Deserializable::from_bytes(
-            &<Vec<u8> as serde::Deserialize>::deserialize(deserializer)?,
-        )
-        .map_err(|err| {
-            serde::de::Error::custom(format!("invalid HPKE tag: {err:?}"))
-        })?))
-    }
-}
-
 pub fn generate() -> (SecretKey, PublicKey) {
     let mut csprng = <rand::rngs::StdRng as rand::SeedableRng>::from_entropy();
     let (sk, pk) = <Kem as hpke::Kem>::gen_keypair(&mut csprng);
-    (SecretKey(Arc::new(sk)), PublicKey(pk))
+    (SecretKey(sk), PublicKey(pk))
 }
