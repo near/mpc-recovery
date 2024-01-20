554de7fabb68e2f139d84e12c82e568d6add8d07
Phuong Nguyen <phuong@near.org>
2024-01-19 19:07:52 -0800

    Added refreshable ReshareProtocol

--- node/src/protocol/cryptography.rs
+++ node/src/protocol/cryptography.rs
@@ -77,8 +77,8 @@ impl CryptographicProtocol for GeneratingState {
                 Ok(action) => action,
                 Err(err) => {
                     drop(protocol);
-                    if let Err(err) = self.protocol.refresh().await {
-                        tracing::warn!(?err, "unable to refresh keygen protocol");
+                    if let Err(refresh_err) = self.protocol.refresh().await {
+                        tracing::warn!(?refresh_err, "unable to refresh keygen protocol");
                     }
                     return Err(err)?;
                 }
@@ -193,7 +193,16 @@ impl CryptographicProtocol for ResharingState {
         tracing::info!("progressing key reshare");
         let mut protocol = self.protocol.write().await;
         loop {
-            let action = protocol.poke()?;
+            let action = match protocol.poke() {
+                Ok(action) => action,
+                Err(err) => {
+                    drop(protocol);
+                    if let Err(refresh_err) = self.protocol.refresh().await {
+                        tracing::warn!(?refresh_err, "unable to refresh reshare protocol");
+                    }
+                    return Err(err)?;
+                }
+            };
             match action {
                 Action::Wait => {
                     drop(protocol);
