2084b60535d9209c9032e403cbc9d032b7736090
Xiangyi Zheng <xiangyi@near.org>
2024-02-05 11:12:42 -0800

    add comment about integration test error

--- integration-tests/tests/multichain/mod.rs
+++ integration-tests/tests/multichain/mod.rs
@@ -41,8 +41,10 @@ async fn test_triples_and_presignatures() -> anyhow::Result<()> {
             // TODO: add test that checks #triples in datastore
             for account_id in state_0.participants.keys() {
                 let triple_storage = ctx.nodes.triple_storage(account_id.to_string()).await?;
+                // This errs out with
+                // Err(GcpError(BadRequest(Object {"error": Object {"code": Number(400), "message": String("Payload isn't valid for request."), "status": String("INVALID_ARGUMENT")}})))
                 let load_res = triple_storage.load().await;
-                print!("result is: {:?}", load_res);
+                //print!("result is: {:?}", load_res);
                 //assert_eq!(load_res.len(), 6);
             }
             Ok(())
