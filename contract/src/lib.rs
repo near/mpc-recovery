3deb009f877cd6c86a7876e03b4bee53554bf275
David Millar-Durrant <dmillardurrant@gmail.com>
2024-05-02 16:37:18 +0200

    Make sure when we fail in the busy loop we cleanup
    We got reports of users getting responses to sign requests on testnet of
    "Too many pending requests. Please, try again later." when the signing
    contract was under no load.
    
    I believe this was caused by someone sending a message that failed, the
    contract cleaning up the request but then immediately panicking. This
    caused the state cleanup to roll back and the system keeping a limit on
    the total number of requests to eventually run out of space.
    
    This PR fixes that by panicking on a seperate call and allowing the
    cleanup to persist.

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -384,21 +384,41 @@ impl MpcContract {
                     PromiseOrValue::Value(signature)
                 }
                 None => {
-                    if depth > 30 {
-                        self.remove_request(&payload);
-                        env::panic_str("Signature was not provided in time. Please, try again.");
+                    // Make sure we have enough gas left to do 1 more call and clean up afterwards
+                    // Observationally 30 calls < 300 TGas so 2 calls < 20 TGas
+                    if depth > 30 && env::prepaid_gas() > Gas::from_tgas(20) {
+                        self.fail(
+                            "Signature was not provided in time. Please, try again.",
+                            payload,
+                        )
+                    } else {
+                        log!(&format!(
+                            "sign_helper: signature not ready yet (depth={})",
+                            depth
+                        ));
+                        let account_id = env::current_account_id();
+                        PromiseOrValue::Promise(
+                            Self::ext(account_id).sign_helper(payload, depth + 1),
+                        )
                     }
-                    log!(&format!(
-                        "sign_helper: signature not ready yet (depth={})",
-                        depth
-                    ));
-                    let account_id = env::current_account_id();
-                    PromiseOrValue::Promise(Self::ext(account_id).sign_helper(payload, depth + 1))
                 }
             }
         } else {
-            env::panic_str("unexpected request");
+            self.fail("unexpected request", payload)
         }
+    }
+
+    /// Cleanup the state and fail
+    /// This allows us to return a panic, without rolling back the state from this call
+    pub fn fail(&mut self, message: &str, payload: [u8; 32]) -> PromiseOrValue<(String, String)> {
+        self.remove_request(&payload);
+        let self_id = env::current_account_id();
+        PromiseOrValue::Promise(Self::ext(self_id).fail_helper(message.to_string()))
+    }
+
+    #[private]
+    pub fn fail_helper(&mut self, message: String) {
+        env::panic_str(&message);
     }
 
     pub fn respond(&mut self, payload: [u8; 32], big_r: String, s: String) {
