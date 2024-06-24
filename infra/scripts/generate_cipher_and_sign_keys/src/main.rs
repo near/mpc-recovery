89fa16c0f5cddf6ccec332fe622cbfd0c2ab8edd
Xiangyi Zheng <xyzheng@uchicago.edu>
2024-06-24 16:21:46 -0700

    add script to generate sign_sk (#654)

--- infra/scripts/generate_cipher_and_sign_keys/src/main.rs
+++ infra/scripts/generate_cipher_and_sign_keys/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let cipher_sk = hex::encode(cipher_sk.to_bytes());
     println!("cipher public key: {}", cipher_pk);
     println!("cipher private key: {}", cipher_sk);
-    let sign_sk: SecretKey = "ed25519:46rYEgLEAQuyHryUaF2R2bXQiQKg5xvbKUzqZSTLZyzK6xDixPaoyex5ab1R5EAaiQCZZovbtzBCA3eQEMKZELWx".parse().unwrap();
+    let sign_sk = near_crypto::SecretKey::from_random(near_crypto::KeyType::ED25519);
     let sign_pk = sign_sk.public_key();
     println!("sign public key sign_pk: {}", sign_pk);
     println!("sign secret key sign_sk: {}", sign_sk);
